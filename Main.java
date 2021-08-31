7453a552cfd0ecca65f37b2ff91b8f83a7f395dc
Rajeshparuchury <89407977+Rajeshparuchury@users.noreply.github.com>
2021-08-31 10:17:50 -0500

    added gradle project 2

---
+++ Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gradleproject2;
+
+/**
+ *
+ * @author S545162
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("1,2");
+        System.out.println("1,2,3");
+        System.out.println("****************");
+        System.out.println("3,2,1");
+        System.out.println("2,1");
+        System.out.println("1");
+       
+        // TODO code application logic here
+    }
+    
+}
